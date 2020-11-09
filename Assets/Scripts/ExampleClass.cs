b9c955265bcf0a30a96b8028c45723b01077093e
Lara <lara.unterreiner@gmail.com>
2020-11-09 14:17:10 +0100

    Git add example class UNTL

---
+++ Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Deklaration einer Klasse
+
+// public           -> access modifier (Zugriffsrechte) | keyword (vorreservierte Schlüsselwörter)
+// class            -> keyword
+// ExampleClass     -> Identifier (Name der Klasse)
+// : MonoBehaviour  -> Erstellen von Unterklasse von MonoBehaviour
+// {}               -> Scope/ Wirkungsbereich (was ist alles in einer klasse enthalten)
+
+
+
+
+
+public class ExampleClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
